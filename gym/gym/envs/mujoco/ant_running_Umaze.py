5ba5b1d4c02b0deffc1ca033b6ae8d48c362f7d3
robin <yt2541@columbia.edu>
2018-03-26 15:38:58 -0400

    randomize init pos and add xml files

--- gym/gym/envs/mujoco/ant_running_Umaze.py
+++ gym/gym/envs/mujoco/ant_running_Umaze.py
@@ -11,6 +11,7 @@ class AntRunUMazeEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         utils.EzPickle.__init__(self)
 
     def _step(self, a):
+        #print(self.sim.data.qpos.size, self.sim.data.qvel.size)
         self.do_simulation(a, self.frame_skip)
         ob = self._get_obs()
         #velocity = self.sim.data.qvel[:2]
@@ -30,8 +31,8 @@ class AntRunUMazeEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         ])
 
     def reset_model(self):
-        qpos = self.init_qpos #+ self.np_random.uniform(size=self.model.nq, low=-.1, high=.1)
-        qvel = self.init_qvel #+ self.np_random.randn(self.model.nv) * .1
+        qpos = self.init_qpos + self.np_random.uniform(size=self.model.nq, low=-.1, high=.1)
+        qvel = self.init_qvel + self.np_random.randn(self.model.nv) * .1
         self.set_state(qpos, qvel)
         return self._get_obs()
 
--- gym/gym/envs/mujoco/ant_running_full.py
+++ gym/gym/envs/mujoco/ant_running_full.py
@@ -11,6 +11,7 @@ class AntRunFullEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         utils.EzPickle.__init__(self)
 
     def _step(self, a):
+        #print(self.sim.data.qpos.size, self.sim.data.qvel.size)
         self.do_simulation(a, self.frame_skip)
         ob = self._get_obs()
         velocity = self.sim.data.qvel[:2]
@@ -26,8 +27,8 @@ class AntRunFullEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         ])
 
     def reset_model(self):
-        qpos = self.init_qpos #+ self.np_random.uniform(size=self.model.nq, low=-.1, high=.1)
-        qvel = self.init_qvel #+ self.np_random.randn(self.model.nv) * .1
+        qpos = self.init_qpos + self.np_random.uniform(size=self.model.nq, low=-.1, high=.1)
+        qvel = self.init_qvel + self.np_random.randn(self.model.nv) * .1
         self.set_state(qpos, qvel)
         return self._get_obs()
 
